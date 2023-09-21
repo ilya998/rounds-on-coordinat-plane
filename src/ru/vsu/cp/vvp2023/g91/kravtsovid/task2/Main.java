423d82599f3fd9fb68d37d94e399643d4608c705
ilya998 <ilya13022006@gmail.com>
2023-09-21 20:23:16 +0300

    err fix

--- src/ru/vsu/cp/vvp2023/g91/kravtsovid/task2/Main.java
+++ src/ru/vsu/cp/vvp2023/g91/kravtsovid/task2/Main.java
@@ -6,17 +6,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Введите координаты от и радиус первой окружности в формате 'x y r': ");
-        int x1 = scan.nextInt();
-        int y1 = scan.nextInt();
+        double x1 = scan.nextInt();
+        double y1 = scan.nextInt();
         double r1 = scan.nextDouble();
         System.out.print("Введите координаты и радиус второй окружности в формате 'x y r': ");
-        int x2 = scan.nextInt();
-        int y2 = scan.nextInt();
+        double x2 = scan.nextInt();
+        double y2 = scan.nextInt();
         double r2 = scan.nextDouble();
-        int x = Math.abs(x1 - x2);
-        int y = Math.abs(y1 - y2);
+        double x = Math.abs(x1 - x2);
+        double y = Math.abs(y1 - y2);
         double dist = Math.sqrt(x * x + y * y);
-        if ((r1 + r2 < dist) || (Math.max(r1, r2) > (Math.min(r1, r2) + dist))) {
+        if (r1 <= 0 || r2 <= 0) {
+            System.out.println("Радиус должен быть положительным.");
+        }else if ((r1 + r2 < dist) || (Math.max(r1, r2) > (Math.min(r1, r2) + dist))) {
             System.out.println("Окружности не пересекаются.");
         } else if ((r1 + r2 == dist) || (Math.max(r1, r2) == Math.min(r1, r2) + dist)) {
             System.out.println("Окружности пересекаются в одной точке.");
